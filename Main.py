56ec4d588cdac964205b407ed0697e6154f893a2
ologoto <kikoliko732@gmail.com>
2021-12-18 00:52:42 +0100

    Loop improved (still need to add escape function).
    Added user selectable frequency of weather request.

--- Main.py
+++ Main.py
@@ -1,35 +1,37 @@
-#import
 import requests
 from pprint import pprint
 import time
-#API
+from time import gmtime, strftime
+
+# API
 API_KEY = 'dbc83d83a2892871b4afdaa3cf4b50af'
 
 print('zadaj mesto')
 
-#city function
+
+# city function
 def city():
     city = input(str())
-    if city == str('vrbove') or city == str('sered'):
+    if city == str('sered'):
         city = str('bratislava')
     else:
         city = city
     return str(city)
 
-#link creation
+
+# link creation
 unites = 'metric'
 lang = 'sk'
 baseurl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city() + '&units=' + unites + '&appid=' + API_KEY
 
-print(baseurl)
+# user selected report frequency
+print('set report frequency (seconds)')
+x = int(input())
 
-i = True
-#loop
-while i:
-    weather_data = requests.get(baseurl).json()
-    pprint(weather_data)
-    print('do ya want to have weather report every 10min, write y or n ?')
-    input(str())
-    if input() == str('y'):
-        time.sleep(1)
-    continue
+# weather report loop
+while baseurl:
+    if True:
+        print(strftime("%d-%m %H:%M:%S", gmtime()))
+        weather_data = requests.get(baseurl).json()
+        pprint(weather_data)
+        time.sleep(x)
