8a0065c5bad14f99e3832e62ecc46ead04dde8e2
ologoto <kikoliko732@gmail.com>
2021-12-18 19:40:33 +0100

    Improved weather report loop
    Notifications added (known bug)

--- Main.py
+++ Main.py
@@ -1,13 +1,14 @@
-import requests
-from pprint import pprint
 import time
+from pprint import pprint
 from time import gmtime, strftime
+from plyer import notification
+
+import requests
 
 # API settings
 API_KEY = 'dbc83d83a2892871b4afdaa3cf4b50af'
 unites = 'metric'
 lang = 'sk'
-
 
 print('zadaj mesto')
 
@@ -23,7 +24,10 @@ def city():
 
 
 # link creation
-baseurl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city() + '&units=' + unites + '&appid=' + API_KEY
+baseurl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city() + '&units=' + unites + '&mode=' + '&appid=' + API_KEY
+# baseurl1 = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city() + '&units=' + unites + '&appid=' + API_KEY
+
+print(baseurl)
 
 # user selected report frequency
 print('set report frequency (minutes)')
@@ -38,6 +42,18 @@ def mintosec():
 # weather report loop
 while baseurl:
     print(strftime("%d-%m %H:%M:%S", gmtime()))
-    weather_data = requests.get(baseurl).json()
-    pprint(weather_data)
-    time.sleep(mintosec())
+    try:
+        weather_data = requests.get(baseurl).json()
+        pprint(weather_data)
+        notification.notify(
+            title='weather report  ' + strftime("%d-%m %H:%M:%S", gmtime()),
+            message=weather_data,
+            app_name='WeatherApp',
+        )
+        time.sleep(mintosec())
+    except RuntimeError or ValueError:
+        break
+
+
+
+
