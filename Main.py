8ef8aa17e01fded227c3a8ae776aa947b9ebce1d
ologoto <kikoliko732@gmail.com>
2021-12-18 13:17:54 +0100

    Loop improved simplified
    Improved user selectable frequency
    
    City function improved

--- Main.py
+++ Main.py
@@ -3,8 +3,11 @@ from pprint import pprint
 import time
 from time import gmtime, strftime
 
-# API
+# API settings
 API_KEY = 'dbc83d83a2892871b4afdaa3cf4b50af'
+unites = 'metric'
+lang = 'sk'
+
 
 print('zadaj mesto')
 
@@ -13,25 +16,28 @@ print('zadaj mesto')
 def city():
     city = input(str())
     if city == str('sered'):
-        city = str('bratislava')
+        city = str('galanta')
     else:
         city = city
     return str(city)
 
 
 # link creation
-unites = 'metric'
-lang = 'sk'
 baseurl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city() + '&units=' + unites + '&appid=' + API_KEY
 
 # user selected report frequency
-print('set report frequency (seconds)')
+print('set report frequency (minutes)')
 x = int(input())
+
+
+def mintosec():
+    i = x * 60
+    return i
+
 
 # weather report loop
 while baseurl:
-    if True:
-        print(strftime("%d-%m %H:%M:%S", gmtime()))
-        weather_data = requests.get(baseurl).json()
-        pprint(weather_data)
-        time.sleep(x)
+    print(strftime("%d-%m %H:%M:%S", gmtime()))
+    weather_data = requests.get(baseurl).json()
+    pprint(weather_data)
+    time.sleep(mintosec())
